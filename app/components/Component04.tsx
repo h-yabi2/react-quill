528efbcd6a656047af91bd82c2d381f17094405d
h-yabi2 <hur4k0dt@gmail.com>
2024-06-25 13:58:27 +0900

    複数ポップアップ対応中

---
+++ app/components/Component04.tsx
@@ -0,0 +1,226 @@
+"use client";
+
+import React, { useMemo, useRef, useEffect, useState } from "react";
+import ReactQuill, { Quill } from "react-quill";
+import "react-quill/dist/quill.snow.css";
+import "@/utils/quill/emoji/custom-emoji";
+import "./Component04.modules.scss";
+
+// ReactQuill の icon をインポート
+const icons = ReactQuill.Quill.import("ui/icons");
+import qlBold from "@/public/img/ql-bold.svg";
+import qlColor from "@/public/img/ql-color.svg";
+import qlBackground from "@/public/img/ql-background.svg";
+import qlEmoji from "@/public/img/ql-emoji.svg";
+import qlSearch from "@/public/img/ql-search.svg";
+import qlEdit from "@/public/img/ql-edit.svg";
+import qlListBullet from "@/public/img/ql-list-bullet.svg";
+import qlListOrdered from "@/public/img/ql-list-ordered.svg";
+
+// デフォルトのアイコンを上書き
+icons["color"] = '<img src="' + qlColor.src + '" alt="" />';
+icons["background"] = '<img src="' + qlBackground.src + '" alt="" />';
+icons["list"]["bullet"] = '<img src="' + qlListBullet.src + '" alt="" />';
+icons["list"]["ordered"] = '<img src="' + qlListOrdered.src + '" alt="" />';
+
+// delta/collectionDetail.ts から取得したデータ
+import { collectionDetail } from "@/delta/collectionDetail";
+
+const Component03: React.FC = () => {
+  const quillRef = useRef<ReactQuill>(null);
+  const [data, setData] = useState(collectionDetail.body);
+
+  const BlockEmbed = Quill.import("blots/embed");
+  interface FileBlotProps {
+    src: string;
+    name: string;
+    size?: string;
+  }
+
+  // 画像
+  class ImgBlot extends BlockEmbed {
+    static create(data: FileBlotProps): any {
+      const node = super.create();
+      node.setAttribute("data-extension", data.name);
+      node.src = data.src;
+      node.alt = "画像";
+      return node;
+    }
+
+    static value(domNode: HTMLIFrameElement): any {
+      return {
+        name: domNode.getAttribute("data-extension"),
+        src: domNode.getAttribute("src"),
+      };
+    }
+  }
+
+  // MEMO: 編集機能からアップされた画像かどうかを区別するため、img_blotとして別途登録
+  ImgBlot.blotName = "img_blot";
+  ImgBlot.tagName = "img";
+  // MEMO:クラス名など、このサイト固有のものとわかる何かをセットしないとコピペの時にすべてのimgがツールバーから貼り付けたものと見なされてしまう。ここ変更かけると既存データの復元ができなくなる可能性があるので注意
+  ImgBlot.className = "nodoCollectionImg";
+  Quill.register(ImgBlot, true);
+
+  const modules = useMemo(() => {
+    return {
+      toolbar: {
+        container: "#toolbar", // id="toorbar"のHTMLエレメントにツールバーを入れる
+        handlers: {
+          popup: () => {},
+        },
+      },
+      "custom-emoji": true,
+    };
+  }, []);
+
+  useEffect(() => {
+    if (quillRef.current && quillRef.current.editor) {
+      quillRef.current.setEditorContents(
+        quillRef.current.editor,
+        JSON.parse(collectionDetail.body)
+      );
+      // 編集可能にする
+      quillRef.current.editor.enable();
+    }
+  }, []);
+
+  // 見出しの矢印を非表示
+  useEffect(() => {
+    const header = document.querySelector(".ql-size");
+    if (header) {
+      const svg = header.querySelector("svg");
+      if (svg) {
+        svg.style.display = "none";
+      }
+    }
+  }, []);
+
+  const handlePopup = (e: React.MouseEvent<HTMLImageElement>) => {
+    const popup = (e.target as Element)?.closest(".ql-popup");
+    if (!popup) return;
+    const popupInner = popup.querySelector(".ql-popup-inner");
+    if (!popupInner) return;
+    const isActive = popupInner.classList.contains("active");
+
+    // すべての .ql-popup-inner の active クラスを削除
+    document.querySelectorAll(".ql-popup-inner").forEach((inner) => {
+      inner.classList.remove("active");
+    });
+
+    // クリックされたボタンの .ql-popup-inner に active クラスをトグル
+    if (!isActive) {
+      popupInner.classList.add("active");
+    }
+
+    // クリック対象以外をクリックしたら .ql-popup-inner の active クラスを削除
+    const handleClickOutside = (event: MouseEvent) => {
+      if (!popup.contains(event.target as Node)) {
+        popupInner.classList.remove("active");
+        document.removeEventListener("click", handleClickOutside);
+      }
+    };
+
+    document.addEventListener("click", handleClickOutside);
+  };
+
+  const handleOnChange = (content: string) => {
+    // html to delta
+    const delta = quillRef.current?.editor?.clipboard.convert(content);
+
+    // setData データ時に { insert: "\n" } を末尾に追加
+    // if (delta && delta.ops) {
+    //   const lastData = delta.ops[delta.ops.length - 1] ?? { insert: "" };
+    //   if (lastData.insert !== "\n") {
+    //     delta.ops.push({ insert: "\n" });
+    //   }
+    // }
+
+    // TODO: 型エラーが出るので、修正が必要
+    setData(delta as any);
+
+    // delta to json
+    // const json = quillRef.current?.editor?.clipboard.convert(delta);
+    // console.log(json);
+  };
+
+  return (
+    <div>
+      <div id="toolbar">
+        <span className="ql-formats">
+          <button className="ql-search">
+            <img src={qlSearch.src} alt="" />
+          </button>
+          <button className="ql-edit">
+            <img src={qlEdit.src} alt="" />
+          </button>
+        </span>
+        <span className="ql-formats">
+          <select className="ql-header ql-size" defaultValue="">
+            <option value="1">見出し（大）</option>
+            <option value="2">見出し（小）</option>
+            <option value="">テキスト</option>
+          </select>
+        </span>
+        <span className="ql-formats ql-popup relative">
+          <img src={qlBold.src} alt="" onClick={handlePopup} />
+          <div className="ql-popup-inner absolute z-10 top-[100%] bg-white border border-gray-300 hidden">
+            <button className="ql-bold">Bold</button>
+            <button className="ql-italic">Italic</button>
+            <button className="ql-underline">Underline</button>
+          </div>
+        </span>
+        <span className="ql-formats">
+          <select className="ql-align" defaultValue="">
+            <option value=""></option>
+            <option value="center"></option>
+            <option value="right"></option>
+          </select>
+        </span>
+        <span className="ql-formats ql-popup relative">
+          <img src={qlListBullet.src} alt="" onClick={handlePopup} />
+          <div className="ql-popup-inner absolute z-10 top-[100%] bg-white border border-gray-300 hidden">
+            <button className="ql-list" value="bullet"></button>
+            <button className="ql-list" value="check"></button>
+            <button className="ql-list" value="ordered"></button>
+          </div>
+        </span>
+        <span className="ql-formats">
+          <select className="ql-color" defaultValue="">
+            <option value="red"></option>
+            <option value="green"></option>
+            <option value="blue"></option>
+            <option value="orange"></option>
+            <option value="yellow"></option>
+            <option></option>
+          </select>
+          <select className="ql-background" defaultValue="">
+            <option value="red"></option>
+            <option value="green"></option>
+            <option value="blue"></option>
+            <option value="orange"></option>
+            <option value="yellow"></option>
+            <option></option>
+          </select>
+          <button className="ql-image" value="idea"></button>
+          <button className="ql-emoji">
+            <img src={qlEmoji.src} alt="" />
+          </button>
+        </span>
+      </div>
+      <ReactQuill
+        ref={quillRef}
+        modules={modules}
+        readOnly={true}
+        theme="snow"
+        onChange={handleOnChange}
+      />
+      {/* 整形データ */}
+      {/* <div>
+        <pre>{JSON.stringify(data, null, 2)}</pre>
+      </div> */}
+    </div>
+  );
+};
+
+export default Component03;
--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,15 @@
 // import MyComponent from "@/app/components/MyComponent";
 // import Component02 from "./components/Component02";
-import Component03 from "./components/Component03";
+// import Component03 from "./components/Component03";
+import Component04 from "./components/Component04";
 
 export default function Home() {
   return (
     <main className="">
       {/* <MyComponent /> */}
       {/* <Component02 /> */}
-      <Component03 />
+      {/* <Component03 /> */}
+      <Component04 />
       {/* test */}
     </main>
   );
